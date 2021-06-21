66e1d05a92694c677f940656c2a0a4d53c6e5674
surajseth90 <surajseth90@gmail.com>
2021-06-21 21:12:37 +0530

    Added rest controller to demonstrate http methods

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+
+	public String name;
+	public long salary;
+
+	public EmployeePayrollDTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
+	}
+
+}
