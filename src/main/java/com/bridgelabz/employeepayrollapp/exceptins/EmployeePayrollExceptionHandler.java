dc6edf1ea54caa56da2f8a45060874eca1bfa92e
surajseth90 <surajseth90@gmail.com>
2021-06-25 12:12:43 +0530

    Added ControllerAdvice to handle exception occures due to invalid name or salary

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptins/EmployeePayrollExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.employeepayrollapp.exceptins;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errorMessage = errorList.stream()
+											.map(objErr -> objErr.getDefaultMessage())
+											.collect(Collectors.toList());
+		ResponseDTO dto = new ResponseDTO("Exception while processing REST Request", errorMessage);
+		return new ResponseEntity<ResponseDTO>(dto, HttpStatus.BAD_REQUEST);
+	}
+}
