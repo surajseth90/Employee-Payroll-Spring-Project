751d67063ba725935da7716cbf30289b284a80f9
surajseth90 <surajseth90@gmail.com>
2021-06-25 12:43:08 +0530

    Added handle exception method occures due to invalid employee Id

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+
+	public EmployeePayrollException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
