fb1aaedc6b544e6af9b38051f6b4896b9b0d4266
surajseth90 <surajseth90@gmail.com>
2021-06-28 11:17:37 +0530

    Refactored Service layer according to mysql database

--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bridgelabz.addressbookapp.exception.AddressBookException;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.exceptions.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 import com.bridgelabz.employeepayrollapp.repo.EmployeeRepo;
 
@@ -15,17 +17,16 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 
 	@Autowired
 	private EmployeeRepo employeeRepo;
-	private List<EmployeePayrollData> employeePayrolllist = new ArrayList<>();
 
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() {
-		return employeePayrolllist;
+		return employeeRepo.findAll();
 	}
 
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataByID(int id) {
-//		return employeePayrolllist.stream().filter(empData->empData.getEmpId()==id).findFirst().orElseThrow();
-		return employeePayrolllist.get(id - 1);
+		return employeeRepo.findById(id)
+						.orElseThrow(() -> new EmployeePayrollException("Emloyee Payroll Book with id = " + id + " is not exists !!"));
 	}
 
 	@Override
@@ -38,15 +39,13 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 	@Override
 	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO dto) {
 		EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataByID(empId);
-		employeePayrollData.setName(dto.getName());
-		employeePayrollData.setSalary(dto.getSalary());
-		employeePayrolllist.set(empId - 1, employeePayrollData);
-		return employeePayrollData;
+		employeePayrollData.updateEmployeePayrollData(dto);
+		return employeeRepo.save(employeePayrollData);
 	}
 
 	@Override
 	public void deleteEmployeePayrollData(int empId) {
-		employeePayrolllist.remove(empId - 1);
+		employeeRepo.deleteById(empId);
 
 	}
 
